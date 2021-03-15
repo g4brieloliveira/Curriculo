6dcfc48351925bf694e5cbf2fafaa0a52fd56124
Gabriel Oliveira <g4bri3l.0lv@gmail.com>
2021-03-15 01:38:33 -0300

    Updating studies page

--- src/views/Studies/index.js
+++ src/views/Studies/index.js
@@ -13,9 +13,10 @@ function Studies() {
           <p>Instituições de ensino</p>
           <Card iDo="Ensino Médio" local="IFMG" description="Sint magna ex minim culpa ipsum irure et ullamco excepteur est ea excepteur."/>
         </div>
-        <div className="divider"></div>
         <div className="courses">
           <p>Cursos</p>
+          <Card iDo="Curso" local="Udemy" description="Cillum incididunt exercitation excepteur magna incididunt. Deserunt anim culpa laboris sit aliquip eiusmod dolore nulla elit anim. Ut aute et qui proident nulla ex labore eu ex consectetur veniam id."/>
+          <Card iDo="Curso" local="Udemy" description="Cillum incididunt exercitation excepteur magna incididunt. Deserunt anim culpa laboris sit aliquip eiusmod dolore nulla elit anim. Ut aute et qui proident nulla ex labore eu ex consectetur veniam id."/>
           <Card iDo="Curso" local="Udemy" description="Cillum incididunt exercitation excepteur magna incididunt. Deserunt anim culpa laboris sit aliquip eiusmod dolore nulla elit anim. Ut aute et qui proident nulla ex labore eu ex consectetur veniam id."/>
         </div>
       </div>
