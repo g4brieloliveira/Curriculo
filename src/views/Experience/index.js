2b43fa28628fcea45a78702f5db1450aaefe888f
Gabriel Oliveira <g4brieloliveira.dev@gmail.com>
2021-04-28 19:26:40 -0300

    adding the coding academy

--- src/views/Experience/index.js
+++ src/views/Experience/index.js
@@ -8,7 +8,7 @@ function Experience() {
     <div className="experience">
       <Header />
       <div className="content">
-        <img id="workImg" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828231.svg?token=exp=1616306570~hmac=9e76523aea3a47b1c729dda2abae344c" alt=""/>
+        <img id="workImg" src="https://www.flaticon.com/svg/vstatic/svg/3143/3143004.svg?token=exp=1619649571~hmac=8600616dcc5eba29eda383bdabaf4022" alt="" />
         <h5>Aba em desenvolvimento, volte em breve para vê-la :)</h5>
       </div>
       <Footer />
--- src/views/Studies/index.js
+++ src/views/Studies/index.js
@@ -11,13 +11,14 @@ function Studies() {
       <div className="content divide">
         <div className="institutions">
           <p>Instituições de ensino</p>
-          <Card iDo="Ensino Médio Técnico" local="IFMG" description="De 2019 até 2021 fiz o meu ensino médio juntamente ao tecnico em informática no Instituto Federal de Educação, Ciência e Tecnologia de Minas Gerais no Campus Ribeirão das Neves"/>
+          <Card iDo="Ensino Médio Técnico" local="IFMG" description="De 2019 até 2021 fiz o meu ensino médio juntamente ao tecnico em informática no Instituto Federal de Educação, Ciência e Tecnologia de Minas Gerais no Campus Ribeirão das Neves" />
+          <Card iDo="Coding Academy" local="Zero to Mastery" description="Ao início de 2021 comecei a fazer cursos da Zero to Mastery Academy nas areas de Front-End e Mobile utilizando ReactJS e React Native respectivamente." />
         </div>
         <div className="courses">
           <p>Cursos</p>
-          <Card iDo="Curso" local="Udemy" description="Em janeiro de 2021 adquiri um curso Fullstack na Udemy em que fiz um projeto completo fazendo a prototipação com o Adobe XD, o desenvolvimento no Backend de uma API utilizanto NodeJs e MongoDB, o desenvolvimento do Frontend com ReactJS e o desenvolvimento da versão mobile com React Native"/>
-          <Card iDo="Curso" local="Rock University" description="Em setembro de 2020 fiz o curso de SEO da Rock Content em que aprendi sobre maneiras de obter melhor rankeamento em indexadores como o Google"/>
-          <Card iDo="Curso" local="JA Brasil" description="Em agosto de 2020 fiz o curso Meu Primeiro Site da Junior Achievement Brasil no qual aprendemos sobre criação de sites com HTML e CSS"/>
+          <Card iDo="Curso" local="Udemy" description="Em janeiro de 2021 adquiri um curso Fullstack na Udemy em que fiz um projeto completo fazendo a prototipação com o Adobe XD, o desenvolvimento no Backend de uma API utilizanto NodeJs e MongoDB, o desenvolvimento do Frontend com ReactJS e o desenvolvimento da versão mobile com React Native" />
+          <Card iDo="Curso" local="Rock University" description="Em setembro de 2020 fiz o curso de SEO da Rock Content em que aprendi sobre maneiras de obter melhor rankeamento em indexadores como o Google" />
+          <Card iDo="Curso" local="JA Brasil" description="Em agosto de 2020 fiz o curso Meu Primeiro Site da Junior Achievement Brasil no qual aprendemos sobre criação de sites com HTML e CSS" />
         </div>
       </div>
       <Footer />
